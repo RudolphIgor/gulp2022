fa6a7c88e491e3fee0787959df5513a4080e262f
Rudolph <azazil47@gmail.com>
2022-09-28 11:18:46 +0700

    Настроен watcher за папкой files

--- gulpfile.js
+++ gulpfile.js
@@ -13,5 +13,13 @@ global.app = {
 //Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
 
+//Наблюдатель за изменениями файлов
+function watcher () {
+	gulp.watch(path.watch.files, copy)
+}
+
+//Построение сценариев выполнения задачь
+const dev = gulp.series(copy,watcher);
+
 //Выполнение сценария по умолчанию
-gulp.task('default', copy);
+gulp.task('default', dev);
