b9674a775c79a0adf08bb9d2afaa93d150e85199
Rudolph <azazil47@gmail.com>
2022-09-28 11:44:06 +0700

    Установлен плагин file-include

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -7,12 +7,15 @@ const srcFolder = `./src`; //Папка с исходниками проекта
 
 export const path = {
 	build: {
+		html:`${buildFolder}/`,
 		files: `${buildFolder}/files/`,
 	},
 	src: {
+		html: `${srcFolder}/*.html`,
 		files: `${srcFolder}/files/**/*.*`,
 	},
 	watch: {
+		html: `${srcFolder}/**/*.html`,
 		files: `${srcFolder}/files/**/*.*`,
 	},
 	clean: buildFolder,
--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,19 @@ global.app = {
 //Импорт задач
 import { copy } from "./gulp/tasks/copy.js";
 import { reset } from "./gulp/tasks/reset.js";
+import { html } from "./gulp/tasks/html.js";
 
 //Наблюдатель за изменениями файлов
 function watcher () {
 	gulp.watch(path.watch.files, copy)
+	gulp.watch(path.watch.html, html)
 }
 
+//Главный сценарий задач
+const mainTasks = gulp.parallel(copy, html);
+
 //Построение сценариев выполнения задачь
-const dev = gulp.series(reset, copy, watcher);
+const dev = gulp.series(reset, mainTasks, watcher);
 
 //Выполнение сценария по умолчанию
 gulp.task('default', dev);
