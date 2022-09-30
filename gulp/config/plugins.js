7cf25882ab55c81baa193b5a94ec6a3fa962eb12
Rudolph <azazil47@gmail.com>
2022-09-30 09:21:08 +0700

    Добавлена нотификация ошибок

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,6 +1,11 @@
 import replace from "gulp-replace"; //Поиск и замена
+import plumber from "gulp-plumber"//Обработка ошибок
+import notify from "gulp-notify"//События подсказки
+import browserSync from "browser-sync"; //Локальный сервер
 
 //Экспортируем объект
 export const plugins = {
 	replace: replace,
+	plumber: plumber,
+	notify: notify,
 }
